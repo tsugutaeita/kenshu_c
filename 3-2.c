5e2f443e9e92f62fb437631dc5ef8a124c9f7a79
eita tsuguta <tsugutaeita@bmcomp.jp>
2024-03-13 16:56:42 +0900

    show process time

--- 3-2.c
+++ 3-2.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<math.h>
+#include<time.h>
 
 int sosu[1000000];
 int sfind(int n){
     int count = 0;
     sosu[0] = 2;
-    for(int i = 3; count < n - 1; i++){
+    for(int i = 3; count < n - 1; i += 2){
         int flag = 1;
         for(int j = 0; sosu[j] <= sqrt(i) && j <= count; j++){
             if(!(i % sosu[j])) {
@@ -21,9 +22,13 @@ int sfind(int n){
     return sosu[count];
 }
 int main(void){
+    clock_t start, end;
     int n, ans;
     scanf("%d", &n);
+    start = clock();
     ans = sfind(n);
     printf("%d\n", ans);
+    end = clock();
+    printf("clock time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
     return 0;
 }
