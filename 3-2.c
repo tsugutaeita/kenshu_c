048239961ca28c02b1a97b23794def3f0c7effd6
eita tsuguta <tsugutaeita@bmcomp.jp>
2024-03-15 10:47:07 +0900

    時短しようと思って素数を予め覚えさせてみたけど、あまり時短にならなかったよ...

--- 3-2.c
+++ 3-2.c
@@ -4,9 +4,12 @@
 
 int sosu[1000000];
 int sfind(int n){
-    int count = 0;
-    sosu[0] = 2;
-    for(int i = 3; count < n - 1; i += 2){
+    int pref[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173};
+    int prefNum = sizeof pref / sizeof pref[0], count = prefNum - 1;
+    for(int i = 0; i < prefNum; i++){
+        sosu[i] = pref[i];
+    }
+    for(int i = pref[prefNum - 1]; count < n; i += 2){
         int flag = 1;
         for(int j = 0; sosu[j] <= sqrt(i) && j <= count; j++){
             if(!(i % sosu[j])) {
@@ -19,6 +22,7 @@ int sfind(int n){
             sosu[count] = i;
         }
     }
+    printf("%d\n", count);
     return sosu[count];
 }
 int main(void){
